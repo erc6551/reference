c4b6c63e3e9dd2604596906000d0a8bc2f8c7230
Jayden Windle <jaydenwindle@gmail.com>
2023-04-09 13:56:49 -0400

    Goerli + Sepolia testnet deployment

--- script/DeployRegistry.s.sol
+++ script/DeployRegistry.s.sol
@@ -8,7 +8,8 @@ import "../src/ERC6551Registry.sol";
 
 contract DeployRegistry is Script {
     function run() external {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("TESTNET_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
         new ERC6551Registry{
             salt: 0x6551655165516551655165516551655165516551655165516551655165516551
