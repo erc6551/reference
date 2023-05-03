f106ba36499fd6e2001006aa0d29ef0af05d1ef2
Jayden Windle <jaydenwindle@gmail.com>
2023-05-03 10:32:40 -0700

    Fixed solidity pragmas

--- src/examples/upgradeable/ERC6551AccountProxy.sol
+++ src/examples/upgradeable/ERC6551AccountProxy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import "openzeppelin-contracts/proxy/ERC1967/ERC1967Upgrade.sol";
 import "openzeppelin-contracts/proxy/Proxy.sol";
