b3a2620429284112eb9bfe4d3ccc5f15e2090b57
Yash Garg <yashygkf@gmail.com>
2023-05-21 02:57:21 +0530

    feat: comments added in NATSPEC format

--- src/ERC6551Registry.sol
+++ src/ERC6551Registry.sol
@@ -9,6 +9,13 @@ import "./lib/ERC6551BytecodeLib.sol";
 contract ERC6551Registry is IERC6551Registry {
     error InitializationFailed();
 
+    /// @dev Function to create the ERC721 Smart Account.
+    /// @param implementation ERC721 Smart Account Contract Address
+    /// @param chainId Chain ID of the blockchain
+    /// @param tokenContract ERC721 contract address
+    /// @param tokenId Token ID of the ERC721
+    /// @param salt Some Randomness
+    /// @param initData Data to be executed on the ERC721 Smart Account
     function createAccount(
         address implementation,
         uint256 chainId,
@@ -17,6 +24,7 @@ contract ERC6551Registry is IERC6551Registry {
         uint256 salt,
         bytes calldata initData
     ) external returns (address) {
+        // Getting the smart contract account byte
         bytes memory code = ERC6551BytecodeLib.getCreationCode(
             implementation,
             chainId,
@@ -25,8 +33,10 @@ contract ERC6551Registry is IERC6551Registry {
             salt
         );
 
+        // Compute the address of the smart contract account
         address _account = Create2.computeAddress(bytes32(salt), keccak256(code));
 
+        // If address returned is not equal to 0, it returns the address else deploy the smart contract account
         if (_account.code.length != 0) return _account;
 
         emit AccountCreated(_account, implementation, chainId, tokenContract, tokenId, salt);
@@ -41,6 +51,13 @@ contract ERC6551Registry is IERC6551Registry {
         return _account;
     }
 
+    /// @dev Function to get the ERC721 Smart Account Address.
+    /// @param implementation ERC721 Smart Account Contract Address
+    /// @param chainId Chain ID of the blockchain
+    /// @param tokenContract ERC721 contract address
+    /// @param tokenId Token ID of the ERC721
+    /// @param salt Some Randomness
+    /// @return address of the ERC721 Smart Account
     function account(
         address implementation,
         uint256 chainId,
