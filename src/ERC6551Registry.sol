37152ca91ffa4624f079fb0616fc107ac1e78885
Vectorized <webby1111@hotmail.com>
2023-10-16 18:19:05 +0000

    Optimize registry

--- src/ERC6551Registry.sol
+++ src/ERC6551Registry.sol
@@ -1,9 +1,107 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/utils/Create2.sol";
-import "./interfaces/IERC6551Registry.sol";
-import "./lib/ERC6551BytecodeLib.sol";
+interface IERC6551Registry {
+    /**
+     * @dev The registry MUST emit the ERC6551AccountCreated event upon successful account creation
+     */
+    event ERC6551AccountCreated(
+        address account,
+        address indexed implementation,
+        bytes32 salt,
+        uint256 chainId,
+        address indexed tokenContract,
+        uint256 indexed tokenId
+    );
+
+    /**
+     * @dev The registry MUST revert with AccountCreationFailed error if the create2 operation fails
+     */
+    error AccountCreationFailed();
+
+    /**
+     * @dev Creates a token bound account for a non-fungible token
+     *
+     * If account has already been created, returns the account address without calling create2
+     *
+     * If initData is not empty and account has not yet been created, calls account with
+     * provided initData after creation
+     *
+     * Emits ERC6551AccountCreated event
+     *
+     * @return the address of the account
+     */
+    function createAccount(
+        address implementation,
+        bytes32 salt,
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId
+    ) external returns (address);
+
+    /**
+     * @dev Returns the computed token bound account address for a non-fungible token
+     *
+     * @return The computed address of the token bound account
+     */
+    function account(
+        address implementation,
+        bytes32 salt,
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId
+    ) external view returns (address);
+}
+
+library ERC6551BytecodeLib {
+    /**
+     * @dev Returns the creation code of the token bound account for a non-fungible token
+     *
+     * @return result The creation code of the token bound account
+     */
+    function getCreationCode(
+        address implementation_,
+        bytes32 salt_,
+        uint256 chainId_,
+        address tokenContract_,
+        uint256 tokenId_
+    ) internal pure returns (bytes memory result) {
+        assembly {
+            result := mload(0x40) // Grab the free memory pointer.
+            // Layout the variables and bytecode backwards.
+            mstore(add(result, 0xb7), tokenId_)
+            mstore(add(result, 0x97), shr(96, shl(96, tokenContract_)))
+            mstore(add(result, 0x77), chainId_)
+            mstore(add(result, 0x57), salt_)
+            mstore(add(result, 0x37), 0x5af43d82803e903d91602b57fd5bf3)
+            mstore(add(result, 0x28), implementation_)
+            mstore(add(result, 0x14), 0x3d60ad80600a3d3981f3363d3d373d3d3d363d73)
+            mstore(result, 0xb7) // Store the length.
+            mstore(0x40, add(result, 0xd7)) // Allocate the memory.
+        }
+    }
+
+    /**
+     * @dev Returns the create2 address computed from `hash`, `salt`, `deployer`.
+     *
+     * @return predicted The create2 address computed from `hash`, `salt`, `deployer`.
+     */
+    function predictDeterministicAddress(bytes32 hash, bytes32 salt, address deployer)
+        internal
+        pure
+        returns (address predicted)
+    {
+        assembly {
+            // Compute and store the bytecode hash.
+            mstore8(0x00, 0xff) // Write the prefix.
+            mstore(0x35, hash)
+            mstore(0x01, shl(96, deployer))
+            mstore(0x15, salt)
+            predicted := keccak256(0x00, 0x55)
+            mstore(0x35, 0) // Restore the overwritten part of the free memory pointer.
+        }
+    }
+}
 
 contract ERC6551Registry is IERC6551Registry {
     /**
@@ -20,7 +118,8 @@ contract ERC6551Registry is IERC6551Registry {
             implementation, salt, chainId, tokenContract, tokenId
         );
 
-        address _account = Create2.computeAddress(salt, keccak256(code));
+        address _account =
+            ERC6551BytecodeLib.predictDeterministicAddress(keccak256(code), salt, address(this));
 
         if (_account.code.length != 0) return _account;
 
@@ -51,6 +150,6 @@ contract ERC6551Registry is IERC6551Registry {
             )
         );
 
-        return Create2.computeAddress(salt, bytecodeHash);
+        return ERC6551BytecodeLib.predictDeterministicAddress(bytecodeHash, salt, address(this));
     }
 }
--- test/ERC6551AccountLib.t.sol
+++ test/ERC6551AccountLib.t.sol
@@ -65,4 +65,21 @@ contract ERC6551AccountLibTest is Test {
 
         assertEq(salt, accountInstance.salt());
     }
+
+    function testERC6551BytecodeLibDifferential(
+        address implementation_,
+        bytes32 salt_,
+        uint256 chainId_,
+        address tokenContract_,
+        uint256 tokenId_
+    ) public {
+        bytes memory computed = ERC6551BytecodeLib.getCreationCode(implementation_, salt_, chainId_, tokenContract_, tokenId_);
+        bytes memory expected = abi.encodePacked(
+            hex"3d60ad80600a3d3981f3363d3d373d3d3d363d73",
+            implementation_,
+            hex"5af43d82803e903d91602b57fd5bf3",
+            abi.encode(salt_, chainId_, tokenContract_, tokenId_)
+        );
+        assertEq(computed, expected);
+    }
 }
