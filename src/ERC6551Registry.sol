b2114da3a3df7106df3552605f0681b28e90c983
Vectorized <webby1111@hotmail.com>
2023-10-18 07:55:35 +0000

    Optimize

--- src/ERC6551Registry.sol
+++ src/ERC6551Registry.sol
@@ -55,11 +55,11 @@ interface IERC6551Registry {
 
 contract ERC6551Registry is IERC6551Registry {
     function createAccount(
-        address, // implementation
-        bytes32, // salt
-        uint256, // chainId
-        address, // tokenContract
-        uint256 // tokenId
+        address implementation,
+        bytes32 salt,
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId
     ) external returns (address) {
         assembly {
             // Memory Layout:
@@ -77,77 +77,86 @@ contract ERC6551Registry is IERC6551Registry {
             // 0xCC   tokenContract (address)        (32 bytes)
             // 0xEC   tokenId (uint256)              (32 bytes)
 
+            // Silence unused variable warnings
+            pop(chainId)
+
             // Copy bytecode + constant data to memory
             calldatacopy(0x8c, 0x24, 0x80) // salt, chainId, tokenContract, tokenId
             mstore(0x6c, 0x5af43d82803e903d91602b57fd5bf3) // ERC-1167 footer
-            calldatacopy(0x69, 0x10, 0x14) // implementation
+            mstore(0x5d, implementation) // implementation
             mstore(0x49, 0x3d60ad80600a3d3981f3363d3d373d3d3d363d73) // ERC-1167 constructor + header
 
             // Copy create2 computation data to memory
             mstore(0x35, keccak256(0x55, 0xb7)) // keccak256(bytedcode)
-            calldatacopy(0x15, 0x24, 0x20) // salt
+            mstore(0x15, salt) // salt
             mstore(0x01, shl(96, address())) // registry address
             mstore8(0x00, 0xff) // 0xFF
 
             // Compute account address
-            let computed := shr(96, shl(96, keccak256(0, 0x55)))
+            let computed := keccak256(0x00, 0x55)
 
-            // Return computed account address if already deployed
-            if gt(extcodesize(computed), 0) {
-                mstore(0x00, computed)
-                return(0x00, 0x20)
+            // Deploy and returns the account address if the account has not been deployed
+            if iszero(extcodesize(computed)) {
+                // Deploy account contract
+                let deployed := create2(0, 0x55, 0xb7, salt)
+
+                // Revert if the deployment fails
+                if iszero(deployed) {
+                    mstore(0x00, 0x20188a59) // `AccountCreationFailed()`
+                    revert(0x1c, 0x04)
+                }
+
+                // Store account address in memory before salt and chainId
+                mstore(0x6c, deployed)
+
+                // Emit the ERC6551AccountCreated event
+                log4(
+                    0x6c,
+                    0x60,
+                    // `ERC6551AccountCreated(address,address,bytes32,uint256,address,uint256)`
+                    0x79f19b3655ee38b1ce526556b7731a20c8f218fbda4a3990b6cc4172fdf88722,
+                    implementation,
+                    tokenContract,
+                    tokenId
+                )
+
+                // Return the account address
+                return(0x6c, 0x20)
             }
 
-            // Deploy account contract
-            let deployed := create2(0, 0x55, 0xb7, calldataload(0x24))
-
-            // Revert if the deployment fails
-            if iszero(deployed) {
-                mstore(0x00, 0x20188a59) // `AccountCreationFailed()`
-                revert(0x1c, 0x04)
-            }
-
-            // Store account address in memory before salt and chainId
-            mstore(0x6c, deployed)
-
-            // Emit the ERC6551AccountCreated event
-            log4(
-                0x6c,
-                0x60,
-                // `ERC6551AccountCreated(address,address,bytes32,uint256,address,uint256)`
-                0x79f19b3655ee38b1ce526556b7731a20c8f218fbda4a3990b6cc4172fdf88722,
-                calldataload(0x04), // implementation
-                calldataload(0x64), // tokenContract
-                calldataload(0x84) // tokenId
-            )
-
-            // Return the account address
-            return(0x6c, 0x20)
+            // Otherwise, return the computed account address
+            mstore(0x00, shr(96, shl(96, computed)))
+            return(0x00, 0x20)
         }
     }
 
     function account(
-        address, // implementation
-        bytes32, // salt
-        uint256, // chainId
-        address, // tokenContract
-        uint256 // tokenId
+        address implementation,
+        bytes32 salt,
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId
     ) external view returns (address) {
         assembly {
+            // Silence unused variable warnings
+            pop(chainId)
+            pop(tokenContract)
+            pop(tokenId)
+
             // Copy bytecode + constant data to memory
             calldatacopy(0x8c, 0x24, 0x80) // salt, chainId, tokenContract, tokenId
             mstore(0x6c, 0x5af43d82803e903d91602b57fd5bf3) // ERC-1167 footer
-            calldatacopy(0x69, 0x10, 0x14) // implementation
+            mstore(0x5d, implementation)
             mstore(0x49, 0x3d60ad80600a3d3981f3363d3d373d3d3d363d73) // ERC-1167 constructor + header
 
             // Copy create2 computation data to memory
             mstore(0x35, keccak256(0x55, 0xb7)) // keccak256(bytedcode)
-            calldatacopy(0x15, 0x24, 0x20) // salt
+            mstore(0x15, salt)
             mstore(0x01, shl(96, address())) // registry address
-            mstore8(0, 0xff) // 0xFF
+            mstore8(0x00, 0xff) // 0xFF
 
             // Store computed account address in memory
-            mstore(0, shr(96, shl(96, keccak256(0, 0x55))))
+            mstore(0x00, shr(96, shl(96, keccak256(0x00, 0x55))))
 
             // Return computed account address
             return(0x00, 0x20)
