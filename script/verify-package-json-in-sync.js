94b4868c2df6ead13ddd7dd4449208417a6d05af
Francesco Sullo <sullof@gmail.com>
2023-10-22 11:46:38 -0700

    Minor fix in verify script

--- script/verify-package-json-in-sync.js
+++ script/verify-package-json-in-sync.js
@@ -4,6 +4,6 @@ const pkg = require("../package.json");
 const pkgc = require("../src/package.json");
 
 if (pkg.version !== pkgc.version) {
-  console.error("package.json and contracts/package.json are out of sync");
+  console.error("package.json and src/package.json are out of sync");
   process.exit(1);
 }
